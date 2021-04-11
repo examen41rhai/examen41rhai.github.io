b82c774271a13cf84c3d121f9a8c09a838a6ce1c
examen41rhai <examen41rhai@gmail.com>
2021-04-11 14:19:35 -0500

    sub archivos

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBN1wUacSmnzT4LtvaSYZ2r2YyuXzddSEA",
+    authDomain: "bdalum415.firebaseapp.com",
+    projectId: "bdalum415",
+    storageBucket: "bdalum415.appspot.com",
+    messagingSenderId: "465216300727",
+    appId: "1:465216300727:web:6894f6bb7f38d968d28f32",
+    measurementId: "G-K17G4JGXMQ"
+});
